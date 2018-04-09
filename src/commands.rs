5931f970d679be9c02da7f512c3a421465e44006
uma <blog3170@gmail.com>
2018-04-10 02:58:41 +0900

    listコマンド時のメッセージを変更

--- src/commands.rs
+++ src/commands.rs
@@ -38,20 +38,24 @@ pub fn delete(path: &Path) -> io::Result<()> {
 }
 
 pub fn list(path: &Path) -> io::Result<()> {
+    let mut has_boiler = false;
     for entry in fs::read_dir(path)? {
         let entry = entry?;
         let path = entry.path();
         if path.is_dir() {
+            has_boiler = true;
             match path.file_name().unwrap().to_str() {
-                Some(name) => println!("‣{}", name),
+                Some(name) => println!("  ‣{}", name),
                 _ => println!("can't read boiler"),
             }
         } else {
             println!("{:?}", path.file_name().unwrap());
         }
     }
+    if !has_boiler {println!("  Nothing boilerplates")};
     Ok(())
 }
+
 pub fn has_boiler(boiler_name: &str, path: &Path) -> io::Result<(bool)> {
     for entry in fs::read_dir(path)? {
         let entry = entry?;
